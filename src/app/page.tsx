22fceb45c714038b97c20e2a6724224740ed819a
ian <ian@soulware.kr>
2024-06-22 17:54:58 +0900

    feat(root): root경로 페이지 정리 및 에러 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,41 @@
-export default function Page() {
-    return <img src="/character.png" alt="character"
-                style={{width: '500px', alignItems: 'center', margin: '0 20%'}}/>
+import Link from "next/link";
+import BoardList from "./board/BoardList";
+
+export default function BoardPage() {
+    return (
+        <>
+            <div id="search-wrap">
+                <input
+                    type="text"
+                    id="search-box"
+                    title="검색어를 입력해 주세요."
+                    placeholder="제목을 입력하세요."
+                    name="board-title"
+                />
+                <button type="button" id="search-btn">
+                    검색
+                </button>
+            </div>
+            <div id="table-wrap">
+                <table>
+                    <colgroup>
+                        <col width="10%" />
+                        <col />
+                        <col width="20%" />
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th scope="col">번호</th>
+                            <th scope="col">제목</th>
+                            <th scope="col">등록일</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <BoardList/>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
