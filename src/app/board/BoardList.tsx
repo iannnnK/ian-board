1b01fceedd3abc435f14fbc09838525a4ecfc83c
ian <ian@soulware.kr>
2024-06-24 11:05:15 +0900

    feat(BoardList): 등록일 포맷 변경

--- src/app/board/BoardList.tsx
+++ src/app/board/BoardList.tsx
@@ -1,26 +1,29 @@
 import Link from "next/link";
 
-interface BoardData {
+interface BoardListData {
     id: number;
     title: string;
+    create_at: string;
     writer: string;
 }
 
 export default async function BoardList() {
-    const boardList = await getBoard();
+    const boardList: BoardListData[] = await getBoard();
 
-    return boardList.map((board: BoardData) => (
+    return boardList.map((board) => (
         <tr key={board.id}>
             <td>{board.id}</td>
             <td>
                 <Link href={`/board/${board.id}`}>{board.title}</Link>
             </td>
-            <td>{board.writer}</td>
+            <td>{board.create_at.substring(0, 10)}</td>
         </tr>
     ));
 }
 
 async function getBoard() {
     const response = await fetch('http://localhost:3000/api/board');
-    return response.json();
+    const data = response.json();
+    console.log(data);
+    return data;
 }
