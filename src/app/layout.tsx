e72383d7dbc22da6cbfc58be0bc6b68c028731ae
ian <ian@soulware.kr>
2024-06-27 14:45:25 +0900

    chore: 코드 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
                     fontSans.variable
                 )}
             >
+                <nav>로그인 로그아웃</nav>
                 {children}
             </body>
         </html>
